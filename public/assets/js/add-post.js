4fa29dc9c33b2d0d469b55fbbaff0bc4a9230131
Ballard Ingram <ballardingram@outlook.com>
2022-06-13 21:05:07 -0500

    CREATING A POST - working, fixed IDs

--- public/assets/js/add-post.js
+++ public/assets/js/add-post.js
@@ -3,13 +3,13 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const body = document.querySelector('textarea[name="post-text"]').value;
+    const text = document.querySelector('textarea[name="post-text"]').value;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
             title:title,
-            body:body
+            post_text: text
         }),
             headers: {
                 'Content-Type': 'application/json'
